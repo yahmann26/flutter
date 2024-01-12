7c2125a21013b5e2bb1dca121cddfa45e54fa815
yahmann26 <ahmadyahman26@gmail.com>
2024-01-12 15:45:56 +0700

    first commit

--- packages/flutter/lib/src/widgets/single_child_scroll_view.dart
+++ packages/flutter/lib/src/widgets/single_child_scroll_view.dart
@@ -1,7 +1,3 @@
-// Copyright 2014 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'dart:math' as math;
 
 import 'package:flutter/gestures.dart' show DragStartBehavior;
@@ -19,125 +15,7 @@ import 'scroll_physics.dart';
 import 'scroll_view.dart';
 import 'scrollable.dart';
 
-/// A box in which a single widget can be scrolled.
-///
-/// This widget is useful when you have a single box that will normally be
-/// entirely visible, for example a clock face in a time picker, but you need to
-/// make sure it can be scrolled if the container gets too small in one axis
-/// (the scroll direction).
-///
-/// It is also useful if you need to shrink-wrap in both axes (the main
-/// scrolling direction as well as the cross axis), as one might see in a dialog
-/// or pop-up menu. In that case, you might pair the [SingleChildScrollView]
-/// with a [ListBody] child.
-///
-/// When you have a list of children and do not require cross-axis
-/// shrink-wrapping behavior, for example a scrolling list that is always the
-/// width of the screen, consider [ListView], which is vastly more efficient
-/// than a [SingleChildScrollView] containing a [ListBody] or [Column] with
-/// many children.
-///
-/// ## Sample code: Using [SingleChildScrollView] with a [Column]
-///
-/// Sometimes a layout is designed around the flexible properties of a
-/// [Column], but there is the concern that in some cases, there might not
-/// be enough room to see the entire contents. This could be because some
-/// devices have unusually small screens, or because the application can
-/// be used in landscape mode where the aspect ratio isn't what was
-/// originally envisioned, or because the application is being shown in a
-/// small window in split-screen mode. In any case, as a result, it might
-/// make sense to wrap the layout in a [SingleChildScrollView].
-///
-/// Doing so, however, usually results in a conflict between the [Column],
-/// which typically tries to grow as big as it can, and the [SingleChildScrollView],
-/// which provides its children with an infinite amount of space.
-///
-/// To resolve this apparent conflict, there are a couple of techniques, as
-/// discussed below. These techniques should only be used when the content is
-/// normally expected to fit on the screen, so that the lazy instantiation of a
-/// sliver-based [ListView] or [CustomScrollView] is not expected to provide any
-/// performance benefit. If the viewport is expected to usually contain content
-/// beyond the dimensions of the screen, then [SingleChildScrollView] would be
-/// very expensive (in which case [ListView] may be a better choice than
-/// [Column]).
-///
-/// ### Centering, spacing, or aligning fixed-height content
-///
-/// If the content has fixed (or intrinsic) dimensions but needs to be spaced out,
-/// centered, or otherwise positioned using the [Flex] layout model of a [Column],
-/// the following technique can be used to provide the [Column] with a minimum
-/// dimension while allowing it to shrink-wrap the contents when there isn't enough
-/// room to apply these spacing or alignment needs.
-///
-/// A [LayoutBuilder] is used to obtain the size of the viewport (implicitly via
-/// the constraints that the [SingleChildScrollView] sees, since viewports
-/// typically grow to fit their maximum height constraint). Then, inside the
-/// scroll view, a [ConstrainedBox] is used to set the minimum height of the
-/// [Column].
-///
-/// The [Column] has no [Expanded] children, so rather than take on the infinite
-/// height from its [BoxConstraints.maxHeight], (the viewport provides no maximum height
-/// constraint), it automatically tries to shrink to fit its children. It cannot
-/// be smaller than its [BoxConstraints.minHeight], though, and It therefore
-/// becomes the bigger of the minimum height provided by the
-/// [ConstrainedBox] and the sum of the heights of the children.
-///
-/// If the children aren't enough to fit that minimum size, the [Column] ends up
-/// with some remaining space to allocate as specified by its
-/// [Column.mainAxisAlignment] argument.
-///
-/// {@tool dartpad}
-/// In this example, the children are spaced out equally, unless there's no more
-/// room, in which case they stack vertically and scroll.
-///
-/// When using this technique, [Expanded] and [Flexible] are not useful, because
-/// in both cases the "available space" is infinite (since this is in a viewport).
-/// The next section describes a technique for providing a maximum height constraint.
-///
-/// ** See code in examples/api/lib/widgets/single_child_scroll_view/single_child_scroll_view.0.dart **
-/// {@end-tool}
-///
-/// ### Expanding content to fit the viewport
-///
-/// The following example builds on the previous one. In addition to providing a
-/// minimum dimension for the child [Column], an [IntrinsicHeight] widget is used
-/// to force the column to be exactly as big as its contents. This constraint
-/// combines with the [ConstrainedBox] constraints discussed previously to ensure
-/// that the column becomes either as big as viewport, or as big as the contents,
-/// whichever is biggest.
-///
-/// Both constraints must be used to get the desired effect. If only the
-/// [IntrinsicHeight] was specified, then the column would not grow to fit the
-/// entire viewport when its children were smaller than the whole screen. If only
-/// the size of the viewport was used, then the [Column] would overflow if the
-/// children were bigger than the viewport.
-///
-/// The widget that is to grow to fit the remaining space so provided is wrapped
-/// in an [Expanded] widget.
-///
-/// This technique is quite expensive, as it more or less requires that the contents
-/// of the viewport be laid out twice (once to find their intrinsic dimensions, and
-/// once to actually lay them out). The number of widgets within the column should
-/// therefore be kept small. Alternatively, subsets of the children that have known
-/// dimensions can be wrapped in a [SizedBox] that has tight vertical constraints,
-/// so that the intrinsic sizing algorithm can short-circuit the computation when it
-/// reaches those parts of the subtree.
-///
-/// {@tool dartpad}
-/// In this example, the column becomes either as big as viewport, or as big as
-/// the contents, whichever is biggest.
-///
-/// ** See code in examples/api/lib/widgets/single_child_scroll_view/single_child_scroll_view.1.dart **
-/// {@end-tool}
-///
-/// See also:
-///
-///  * [ListView], which handles multiple children in a scrolling list.
-///  * [GridView], which handles multiple children in a scrolling grid.
-///  * [PageView], for a scrollable that works page by page.
-///  * [Scrollable], which handles arbitrary scrolling effects.
 class SingleChildScrollView extends StatelessWidget {
-  /// Creates a box in which a single widget can be scrolled.
   const SingleChildScrollView({
     super.key,
     this.scrollDirection = Axis.vertical,
@@ -152,78 +30,37 @@ class SingleChildScrollView extends StatelessWidget {
     this.restorationId,
     this.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.manual,
   }) : assert(
-         !(controller != null && (primary ?? false)),
-         'Primary ScrollViews obtain their ScrollController via inheritance '
-         'from a PrimaryScrollController widget. You cannot both set primary to '
-         'true and pass an explicit controller.',
-       );
+          !(controller != null && (primary ?? false)),
+          'Primary ScrollViews obtain their ScrollController via inheritance '
+          'from a PrimaryScrollController widget. You cannot both set primary to '
+          'true and pass an explicit controller.',
+        );
 
-  /// {@macro flutter.widgets.scroll_view.scrollDirection}
   final Axis scrollDirection;
 
-  /// Whether the scroll view scrolls in the reading direction.
-  ///
-  /// For example, if the reading direction is left-to-right and
-  /// [scrollDirection] is [Axis.horizontal], then the scroll view scrolls from
-  /// left to right when [reverse] is false and from right to left when
-  /// [reverse] is true.
-  ///
-  /// Similarly, if [scrollDirection] is [Axis.vertical], then the scroll view
-  /// scrolls from top to bottom when [reverse] is false and from bottom to top
-  /// when [reverse] is true.
-  ///
-  /// Defaults to false.
   final bool reverse;
 
-  /// The amount of space by which to inset the child.
   final EdgeInsetsGeometry? padding;
 
-  /// An object that can be used to control the position to which this scroll
-  /// view is scrolled.
-  ///
-  /// Must be null if [primary] is true.
-  ///
-  /// A [ScrollController] serves several purposes. It can be used to control
-  /// the initial scroll position (see [ScrollController.initialScrollOffset]).
-  /// It can be used to control whether the scroll view should automatically
-  /// save and restore its scroll position in the [PageStorage] (see
-  /// [ScrollController.keepScrollOffset]). It can be used to read the current
-  /// scroll position (see [ScrollController.offset]), or change it (see
-  /// [ScrollController.animateTo]).
   final ScrollController? controller;
 
-  /// {@macro flutter.widgets.scroll_view.primary}
   final bool? primary;
 
-  /// How the scroll view should respond to user input.
-  ///
-  /// For example, determines how the scroll view continues to animate after the
-  /// user stops dragging the scroll view.
-  ///
-  /// Defaults to matching platform conventions.
   final ScrollPhysics? physics;
 
-  /// The widget that scrolls.
-  ///
-  /// {@macro flutter.widgets.ProxyWidget.child}
   final Widget? child;
 
-  /// {@macro flutter.widgets.scrollable.dragStartBehavior}
   final DragStartBehavior dragStartBehavior;
 
-  /// {@macro flutter.material.Material.clipBehavior}
-  ///
-  /// Defaults to [Clip.hardEdge].
   final Clip clipBehavior;
 
-  /// {@macro flutter.widgets.scrollable.restorationId}
   final String? restorationId;
 
-  /// {@macro flutter.widgets.scroll_view.keyboardDismissBehavior}
   final ScrollViewKeyboardDismissBehavior keyboardDismissBehavior;
 
   AxisDirection _getDirection(BuildContext context) {
-    return getAxisDirectionFromAxisReverseAndDirectionality(context, scrollDirection, reverse);
+    return getAxisDirectionFromAxisReverseAndDirectionality(
+        context, scrollDirection, reverse);
   }
 
   @override
@@ -233,8 +70,9 @@ class SingleChildScrollView extends StatelessWidget {
     if (padding != null) {
       contents = Padding(padding: padding!, child: contents);
     }
-    final bool effectivePrimary = primary
-        ?? controller == null && PrimaryScrollController.shouldInherit(context, scrollDirection);
+    final bool effectivePrimary = primary ??
+        controller == null &&
+            PrimaryScrollController.shouldInherit(context, scrollDirection);
 
     final ScrollController? scrollController = effectivePrimary
         ? PrimaryScrollController.maybeOf(context)
@@ -270,10 +108,8 @@ class SingleChildScrollView extends StatelessWidget {
     }
 
     return effectivePrimary && scrollController != null
-      // Further descendant ScrollViews will not inherit the same
-      // PrimaryScrollController
-      ? PrimaryScrollController.none(child: scrollable)
-      : scrollable;
+        ? PrimaryScrollController.none(child: scrollable)
+        : scrollable;
   }
 }
 
@@ -299,8 +135,8 @@ class _SingleChildViewport extends SingleChildRenderObjectWidget {
   }
 
   @override
-  void updateRenderObject(BuildContext context, _RenderSingleChildViewport renderObject) {
-    // Order dependency: The offset setter reads the axis direction.
+  void updateRenderObject(
+      BuildContext context, _RenderSingleChildViewport renderObject) {
     renderObject
       ..axisDirection = axisDirection
       ..offset = offset
@@ -313,19 +149,22 @@ class _SingleChildViewport extends SingleChildRenderObjectWidget {
   }
 }
 
-class _SingleChildViewportElement extends SingleChildRenderObjectElement with NotifiableElementMixin, ViewportElementMixin {
+class _SingleChildViewportElement extends SingleChildRenderObjectElement
+    with NotifiableElementMixin, ViewportElementMixin {
   _SingleChildViewportElement(_SingleChildViewport super.widget);
 }
 
-class _RenderSingleChildViewport extends RenderBox with RenderObjectWithChildMixin<RenderBox> implements RenderAbstractViewport {
+class _RenderSingleChildViewport extends RenderBox
+    with RenderObjectWithChildMixin<RenderBox>
+    implements RenderAbstractViewport {
   _RenderSingleChildViewport({
     AxisDirection axisDirection = AxisDirection.down,
     required ViewportOffset offset,
     RenderBox? child,
     required Clip clipBehavior,
-  }) : _axisDirection = axisDirection,
-       _offset = offset,
-       _clipBehavior = clipBehavior {
+  })  : _axisDirection = axisDirection,
+        _offset = offset,
+        _clipBehavior = clipBehavior {
     this.child = child;
   }
 
@@ -357,9 +196,6 @@ class _RenderSingleChildViewport extends RenderBox with RenderObjectWithChildMix
     markNeedsLayout();
   }
 
-  /// {@macro flutter.material.Material.clipBehavior}
-  ///
-  /// Defaults to [Clip.none], and must not be null.
   Clip get clipBehavior => _clipBehavior;
   Clip _clipBehavior = Clip.none;
   set clipBehavior(Clip value) {
@@ -377,8 +213,6 @@ class _RenderSingleChildViewport extends RenderBox with RenderObjectWithChildMix
 
   @override
   void setupParentData(RenderObject child) {
-    // We don't actually use the offset argument in BoxParentData, so let's
-    // avoid allocating it at all.
     if (child.parentData is! ParentData) {
       child.parentData = ParentData();
     }
@@ -468,17 +302,13 @@ class _RenderSingleChildViewport extends RenderBox with RenderObjectWithChildMix
     return 0.0;
   }
 
-  // We don't override computeDistanceToActualBaseline(), because we
-  // want the default behavior (returning null). Otherwise, as you
-  // scroll, it would shift in its parent if the parent was baseline-aligned,
-  // which makes no sense.
-
   @override
   Size computeDryLayout(BoxConstraints constraints) {
     if (child == null) {
       return constraints.smallest;
     }
-    final Size childSize = child!.getDryLayout(_getInnerConstraints(constraints));
+    final Size childSize =
+        child!.getDryLayout(_getInnerConstraints(constraints));
     return constraints.constrain(childSize);
   }
 
@@ -520,9 +350,9 @@ class _RenderSingleChildViewport extends RenderBox with RenderObjectWithChildMix
       case Clip.antiAlias:
       case Clip.antiAliasWithSaveLayer:
         return paintOffset.dx < 0 ||
-               paintOffset.dy < 0 ||
-               paintOffset.dx + child!.size.width > size.width ||
-               paintOffset.dy + child!.size.height > size.height;
+            paintOffset.dy < 0 ||
+            paintOffset.dx + child!.size.width > size.width ||
+            paintOffset.dy + child!.size.height > size.height;
     }
   }
 
@@ -551,7 +381,8 @@ class _RenderSingleChildViewport extends RenderBox with RenderObjectWithChildMix
     }
   }
 
-  final LayerHandle<ClipRectLayer> _clipRectLayer = LayerHandle<ClipRectLayer>();
+  final LayerHandle<ClipRectLayer> _clipRectLayer =
+      LayerHandle<ClipRectLayer>();
 
   @override
   void dispose() {
@@ -574,7 +405,7 @@ class _RenderSingleChildViewport extends RenderBox with RenderObjectWithChildMix
   }
 
   @override
-  bool hitTestChildren(BoxHitTestResult result, { required Offset position }) {
+  bool hitTestChildren(BoxHitTestResult result, {required Offset position}) {
     if (child != null) {
       return result.addWithPaintOffset(
         offset: _paintOffset,
@@ -589,7 +420,8 @@ class _RenderSingleChildViewport extends RenderBox with RenderObjectWithChildMix
   }
 
   @override
-  RevealedOffset getOffsetToReveal(RenderObject target, double alignment, { Rect? rect }) {
+  RevealedOffset getOffsetToReveal(RenderObject target, double alignment,
+      {Rect? rect}) {
     rect ??= target.paintBounds;
     if (target is! RenderBox) {
       return RevealedOffset(offset: offset.pixels, rect: rect);
@@ -623,7 +455,8 @@ class _RenderSingleChildViewport extends RenderBox with RenderObjectWithChildMix
         targetMainAxisExtent = bounds.width;
     }
 
-    final double targetOffset = leadingScrollOffset - (mainAxisExtent - targetMainAxisExtent) * alignment;
+    final double targetOffset = leadingScrollOffset -
+        (mainAxisExtent - targetMainAxisExtent) * alignment;
     final Rect targetRect = bounds.shift(_paintOffsetForPosition(targetOffset));
     return RevealedOffset(offset: targetOffset, rect: targetRect);
   }
